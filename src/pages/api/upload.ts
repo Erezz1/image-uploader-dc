9f9eed24209def59556868e65794f399ca76f981
Ernesto Perez <ernestoperez@STF-327-AN.local>
2023-03-17 11:38:44 -0600

    Funcion para eliminar archivos de la carpeta temporal agregada

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,21 +1,25 @@
+import fs from 'fs';
+import { promisify } from 'util';
 import multer from 'multer';
 import nc from 'next-connect';
 import { v2 as cloudinary } from 'cloudinary';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+const unlinkAsync = promisify(fs.unlink);
 cloudinary.config(process.env.CLOUDINARY_URL!);
 
 // Multer middleware
 const upload = multer({
   storage: multer.diskStorage({
-    destination: 'tmp/uploads',
+    destination: './tmp/uploads',
     filename: (req, file, cb) => cb(null, file.originalname)
   }),
 });
 
 // Catch error and not found api
 const apiRoute = nc({
-  onError: (err, req, res: NextApiResponse) => {
+  onError: (error, req, res: NextApiResponse) => {
+    console.log(error);
     res.status(500).json({ msg: 'Something broke!' });
   },
   onNoMatch: (req, res: NextApiResponse) => {
@@ -35,6 +39,7 @@ apiRoute.post(async (
     const image = await cloudinary.uploader.upload(req.file.path, {
       folder: 'image-uploader'
     });
+    await unlinkAsync(req.file.path);
     res.status(200).json({ url: image.url });
   } catch (error) {
     console.log(error);
